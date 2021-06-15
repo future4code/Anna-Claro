b0c0ad5988a3e7ca8309bc400c9db3f5d7654e67
Your Name <you@example.com>
2021-06-15 15:31:02 -0300

    exercicios dos operadores

---
+++ semana1/operadores/index.js
@@ -0,0 +1,73 @@
+// Exercício 1
+/*const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", false)
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", false) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", true)
+
+console.log("d. ", typeof boolean)*/
+
+// Exercício 2/3
+/* Estava retornando como resultado os dois números concatenados, e nao a soma. Resolvi da seguinte forma:
+
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = Number(primeiroNumero) + Number(segundoNumero)
+
+console.log(soma)*/
+
+// Exercicio 1
+
+/* let idade = Number(prompt("Qual a sua idade?"))
+let idadeAmigo = Number(prompt("Qual a idade do seu melhor amigo(a)?"))
+
+let resultado = idade >= idadeAmigo
+
+console.log ("Sua idade é maior do que a do seu melhor amigo?", resultado) 
+
+let restoDivi = idade % idadeAmigo
+
+console.log (restoDivi)
+
+Exercicio 2
+
+let numPar = Number(prompt("Insira um número par."))
+
+let result = numPar % 2
+
+console.log(result)  se inserido um número impar ele retornará o resto da divisão 1 */
+
+// Exercicio 3
+
+/*let idade = Number(prompt("Qual a sua idade?"))
+
+let idadeMeses = idade * 12
+console.log(idadeMeses)
+
+let idadeDias = idade * 365
+console.log(idadeDias)
+
+let idadeHoras = idade * 8760
+console.log(idadeHoras)*/
+
+// Exercicio 4
+
+/*let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!"))
+
+let a = primeiroNumero > segundoNumero 
+console.log(a)
+let b = primeiroNumero === segundoNumero 
+console.log(b)
+let c = primeiroNumero >= segundoNumero 
+console.log(c)
+let d = segundoNumero <= primeiroNumero 
+console.log(d)*/

7dc20f018487d20737b70e1058269e2693ae2473
Your Name <you@example.com>
2021-06-17 16:30:25 -0300

    funcoes

---
+++ semana1/funcoes/index.js
@@ -0,0 +1,74 @@
+/* 10, 50 - Nao apareceria nada no console 
+
+Essa função verifica se contem a palavra cenoura na entrada
+Eu gosto de cenoura - true 
+CENOURA é bom pra vista - true
+Cenouras crescem na terra - true*/
+
+// Exercicios de escrita de código
+
+/*function sobreMim() {
+    const texto = "Eu sou a Liz, tenho 24 anos, moro em São Paulo e sou estudante."
+    console.log(texto)
+}
+sobreMim()
+
+
+const nome = prompt("Qual é o seu nome?")
+const idade = Number(prompt("Qual é a sua idade?"))
+const cidade = prompt("Qual é a sua cidade?")
+const profissao = prompt("Qual é a sua profissão?")
+function informacoes(nome, idade, cidade, profissao){
+    console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}`)
+}
+informacoes
+
+function somaNumeros(num1, num2){
+    return num1 + num2
+    const somaFinal = somaNumeros(2, 3)
+}
+console.log(somaNumeros(2, 3))
+
+function maiorOuIgual(num1, num2){
+    return num1 >= num2
+}
+const resultadoFinal = maiorOuIgual(5,10)
+console.log(resultadoFinal)
+
+function numeroPar(num) {
+    return num % 2 == 0;
+  }
+ const resultado = numeroPar(4)
+ console.log(resultado)
+
+
+ const texto = prompt("Escreva uma frase")
+ function frase(texto){
+   console.log(texto.length)
+   return texto.toUpperCase()
+}
+const final = frase(texto)
+console.log(final)
+
+
+const num1 = Number(prompt("Qual é a sua idade?"))
+const num2 = Number(prompt("Qual é a sua idade?"))
+function somaNumeros(num1, num2){
+    return num1 + num2
+}
+console.log(somaNumeros(num1, num2))
+
+function subtracaoNumeros(num1, num2){
+    return num1 - num2
+}
+console.log(subtracaoNumeros(num1, num2))
+
+function multiplicacaoNumeros(num1, num2){
+    return num1 * num2
+}
+console.log(multiplicacaoNumeros(num1, num2))
+
+function divisaoNumeros(num1, num2){
+    return num1 / num2
+}
+console.log(divisaoNumeros(num1, num2))*/
